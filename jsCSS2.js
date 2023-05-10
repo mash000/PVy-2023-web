74242865745f2fc4d9c5c906d3b71c0825fa7194
mash000 <113502004+mash000@users.noreply.github.com>
2023-05-10 08:39:17 +0200

    Version 1

---
+++ jsCSS2.js
@@ -0,0 +1,6 @@
+/*document.querySelector(".menu").addEventListener('click', () => {
+    document.querySelector(".menu-polozky").classList.toggle("skryte");
+});*/
+document.querySelector(".submit").addEventListener('click', () => {
+    document.querySelector(".popup-overlay").classList.toggle("skryte");
+});
